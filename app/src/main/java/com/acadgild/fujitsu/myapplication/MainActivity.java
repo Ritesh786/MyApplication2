551caf9af221239e5c7324934304b6a708610904
Ritesh Jha <jharitesh6@gmail.com>
2017-05-15 15:16:21 +0530

    Initial commit

---
+++ app/src/main/java/com/acadgild/fujitsu/myapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.acadgild.fujitsu.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button msetting;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        msetting = (Button) findViewById(R.id.button);    //Button for open Setting
+        msetting.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Intent intent = new Intent(this,SettingsActivity.class);
+        startActivity(intent);
+
+    }
+}
